9b8f1649c8ead06dc597b2d61493f48489681f45
unknown <bhattacharjeesumon93@gmail.com>
2020-07-17 21:15:26 +0530

    first commit

---
+++ p2/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("HELLO DINGA")
+    
+def home(request):
+    return HttpResponse("<h1>Welcome to Home Page<h1>")
+
+def html_demo2(request):
+    return render(request,"sample05.html")
